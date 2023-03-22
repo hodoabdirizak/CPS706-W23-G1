0fd1b825b873e5fb3c5e2dfb7f06401b018f8cf2
camillia <hanaan.amin@yahoo.com>
2023-03-22 01:19:49 -0400

    input validation

--- centralized/test.py
+++ centralized/test.py
@@ -19,7 +19,7 @@ offline_routers_var = tk.StringVar()
 num_routers_label = tk.Label(window, text = 'Number of Routers', font=('calibre', 10, 'bold'))
 source_router_label = tk.Label(window, text = 'Source Router', font=('calibre', 10, 'bold'))
 dest_router_label = tk.Label(window, text = 'Destination Router', font=('calibre', 10, 'bold'))
-offline_routers_label = tk.Label(window, text = 'List of Offline Routers', font=('calibre', 10, 'bold'))
+offline_routers_label = tk.Label(window, text = 'List of Offline Routers (optional)', font=('calibre', 10, 'bold'))
 
 # entries for vars
 num_routers_entry = tk.Entry(window, textvariable = num_routers_var, font=('calibre',10,'normal'))
@@ -28,45 +28,67 @@ dest_router_entry = tk.Entry(window, textvariable = dest_router_var, font=('cali
 offline_routers_entry = tk.Entry(window, textvariable = offline_routers_var, font=('calibre',10,'normal'))
 
 # ERROR CHECKING - not done
+def print_errors():
+    # create canvas for displaying error msgs
+    canvas = tk.Canvas(window, width= 750, height= 150, bg="White")
+
+    if num_routers_var.get() == "" or source_router_var.get() == "" or dest_router_var.get() == "":
+        canvas.create_text(10,10, anchor='nw', text="Complete all fields", fill="red", font=('calibre 10 bold'))
+        canvas.grid(row=5,column=0, columnspan = 10, sticky = tk.W+tk.E)
+        return True
+    
+    else:
+        num_routers = int(num_routers_var.get())
+        source_router = int(source_router_var.get())
+        dest_router = int(dest_router_var.get())
+        offline_routers_raw = offline_routers_var.get()
+
+        if not 3 <= num_routers <= 15:
+            canvas.create_text(10,10, anchor='nw', text="Change num_routers to a value between 3 and 15", fill="red", font=('calibre 10 bold'))
+            canvas.grid(row=5,column=0, columnspan = 10, sticky = tk.W+tk.E)
+            return True
+        
+        elif source_router == dest_router:
+            canvas.create_text(10,10, anchor='nw', text="source_router and dest_router must be different".format(num_routers), fill="red", font=('calibre 10 bold'))
+            canvas.grid(row=5,column=0, columnspan = 10, sticky = tk.W+tk.E)
+            return True
+
+        elif source_router > num_routers or source_router < 1 or dest_router > num_routers or dest_router < 1:
+            canvas.create_text(10,10, anchor='nw', text="Change source_router and dest_router to a value between 1 and {}".format(num_routers), fill="red", font=('calibre 10 bold'))
+            canvas.grid(row=5,column=0, columnspan = 10, sticky = tk.W+tk.E)
+            return True
+
+        elif not (re.search("^[-,0-9]+$",offline_routers_raw) or offline_routers_raw == ""):
+            canvas.create_text(10,10, anchor='nw', text="offline_routers must follow format: 1,2,3", fill="red", font=('calibre 10 bold')) 
+            canvas.grid(row=5,column=0, columnspan = 10, sticky = tk.W+tk.E)
+            return True
+
+        if offline_routers_raw == "":
+            offline_routers = None
+
+        elif re.search("^[-,0-9]+$",offline_routers_raw):
+            offline_routers = [int(router) for router in offline_routers_raw.split(",")]
+
+        elif source_router in offline_routers:
+            canvas.create_text(10,10, anchor='nw', text="Change source router or remove source router from list", fill="red", font=('calibre 10 bold'))  
+            canvas.grid(row=5,column=0, columnspan = 10, sticky = tk.W+tk.E)  
+            return True
+
+        elif dest_router in offline_routers:
+            canvas.create_text(10,10, anchor='nw', text="Change dest router or remove dest router from list", fill="red", font=('calibre 10 bold'))
+            canvas.grid(row=5,column=0, columnspan = 10, sticky = tk.W+tk.E)
+            return True
+        else:
+            return False    
+    
+
 def validate():
     '''validates that info provided is correct
     if invalid: print error msg to window 
     else: call edges()'''
-    num_routers = int(num_routers_var.get())
-    source_router = int(source_router_var.get())
-    dest_router = int(dest_router_var.get())
-    offline_routers_raw = offline_routers_var.get()
 
-    # create canvas for displaying error msgs
-    canvas= tk.Canvas(window, width= 300, height= 150, bg="White")
-
-    if not 3 < num_routers < 15:
-        canvas.create_text(200, 50, text="Change num_routers to a value between 3 and 15", fill="red", font=('calibre 10 bold'))
-    
-    elif source_router > num_routers or source_router < 1 or dest_router > num_routers or dest_router < 1:
-        canvas.create_text(200, 50, text="Change source_router and dest_router to a value between 1 and {}".format(num_routers), fill="red", font=('calibre 10 bold'))
-
-    elif not re.search("^[-,0-9]+$",offline_routers_raw) or not offline_routers_raw == "":
-        canvas.create_text(200, 50, text="offline_routers must follow format: 1,2,3", fill="red", font=('calibre 10 bold')) 
-
-    if offline_routers_raw == "":
-        offline_routers = None
-
-    elif re.search("^[-,0-9]+$",offline_routers_raw):
-        offline_routers = [int(router) for router in offline_routers_raw.split(",")]
-
-    elif source_router in offline_routers:
-        canvas.create_text(200, 50, text="Change source router or remove source router from list", fill="red", font=('calibre 10 bold'))    
-
-    elif dest_router in offline_routers:
-        canvas.create_text(200, 50, text="Change dest router or remove dest router from list", fill="red", font=('calibre 10 bold'))    
-    
-    # other errors im prob missing
-
-    else:
-        edges()
-
-    canvas.pack()
+    if not print_errors():
+        edges()       
 
 sub_btn=tk.Button(window,text = 'Submit', command = validate)
 
@@ -75,8 +97,8 @@ def edges():
     allow user to change cost of each edge'''
     # create canvas for displaying error msgs
     canvas = tk.Canvas(window, width= 300, height= 150, bg="White")
-    canvas.create_text(200, 50, text="edges fxn works", fill="red", font=('calibre 10 bold'))  
-    canvas.pack()
+    canvas.create_text(10,10, anchor='nw', text="print undirected graph here", fill="red", font=('calibre 10 bold'))  
+    canvas.grid(row=5,column=0, columnspan = 5, sticky = tk.W+tk.E)
     
     # allow user to input cost for each edge OR randomize costs
 
