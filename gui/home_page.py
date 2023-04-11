1e407bb6f6e512d1c5bc6b01580a061a471330eb
camillia <hanaan.amin@yahoo.com>
2023-04-10 22:13:26 -0400

    edited error message

--- gui/home_page.py
+++ gui/home_page.py
@@ -16,12 +16,15 @@ def print_errors(A,B,C):
         dest_router = int(C)
 
         if not 3 <= num_routers <= 15:
-            return "Change num_routers to a value between 3 and 15"
+            return "Number of routers must be between 3 and 15"
         
         elif source_router == dest_router:
-            return "source_router and dest_router must be different"
+            return "Source and destination router must be different"
 
-        elif source_router > num_routers or source_router < 1 or dest_router > num_routers or dest_router < 1:
-            return "Change source_router and dest_router to a value between 1 and {}".format(num_routers)
+        elif source_router > num_routers or source_router < 1:
+            return "Change source router to a value between 1 and {}".format(num_routers)
+        
+        elif dest_router > num_routers or dest_router < 1:
+            return "Change destination router to a value between 1 and {}".format(num_routers)
 
         return None
